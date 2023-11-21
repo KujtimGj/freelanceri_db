0f3cee6be5fba89a582d3b745bb168d8a4c12ed6
KujtimGj <kujtim.gjokaj@harrisia.com>
2023-11-21 11:21:39 +0100

    data inside token

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,47 +1,59 @@
-const User = require('../models/userModel')
-const jwt = require('jsonwebtoken')
+const User = require("../models/userModel");
+const jwt = require("jsonwebtoken");
 
+// create webtoken
+const createToken = (_id, email, firstName, lastName) => {
+  return jwt.sign({ _id, email, firstName, lastName }, process.env.SECRET, {
+    expiresIn: "3d",
+  });
+};
 
+// login user
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.login(email, password);
 
-//create webtoken
-const createToken=(_id)=>{
-   return jwt.sign({_id:_id},process.env.SECRET,{expiresIn:'3d'})
-}
+    const token = createToken(
+      user._id,
+      user.email,
+      user.firstName,
+      user.lastName
+    );
 
+    res
+      .status(200)
+      .json({
+        email,
+        token,
+        firstName: user.firstName,
+        lastName: user.lastName,
+      });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
+// signup user
+const signUpUser = async (req, res) => {
+  const { firstName, lastName, email, password } = req.body;
 
-//login user
-const loginUser = async(req,res)=>{
-    const {email, password}=req.body
-    try {
-        const user = await User.login(email,password)
+  try {
+    const user = await User.signup(firstName, lastName, email, password);
+    const token = createToken(
+      user._id,
+      user.email,
+      user.firstName,
+      user.lastName
+    );
 
-        const token = createToken(user._id)
-        
-        res.status(200).json({email,token})
-    } catch (error) {
-        res.status(400).json({error:error.message})
-    }
-}
+    res.status(200).json({email,token,firstName: user.firstName,lastName: user.lastName });
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+};
 
-
-//signup user1
-const signUpUser = async(req,res)=>{
-    const {firstName,lastName,email,password}=req.body
-
-    try {
-        const user = await User.signup(firstName,lastName,email,password)
-        const token = createToken(user._id)
-        res.status(200).json({email,token})
-        var ress = res.status({status:true,success:"Registered successfully"})
-        console.log(ress)
-    } catch (error) {
-        res.status(400).json({error:error.message})
-    }
-}
-
-
-module.exports={
-    loginUser,
-    signUpUser
-}
+module.exports = {
+  loginUser,
+  signUpUser,
+};
