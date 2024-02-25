7284f3aeb9175665a6eff9be4a012b60a12154d8
KujtimGj <kujtim.gjokaj@harrisia.com>
2024-02-25 16:20:22 +0100

    auth middleware

--- routes/contactRoute.js
+++ routes/contactRoute.js
@@ -2,10 +2,11 @@ const express = require("express");
 const router = express.Router();
 
 const {getContact,getContacts,createContact} = require("../controllers/contactUsController");
+const authMiddleware = require("../middlewares/authMiddleware");
 
 
-router.get("/:id",getContact);
-router.get("/",getContacts);
+router.get("/:id",authMiddleware,getContact);
+router.get("/",authMiddleware,getContacts);
 router.post("/",createContact)
 
 module.exports=router
