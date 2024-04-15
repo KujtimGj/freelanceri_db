afdc4269fa8326b95ff9ab1d22f90665d7c779d0
KujtimGj <kujtim.gjokaj@harrisia.com>
2024-04-16 00:25:57 +0200

    asd

--- app.js
+++ app.js
@@ -64,6 +64,8 @@ app.use("/rating", rating);
 app.use("/bookmark", bookmark);
 app.use("/contract", contract);
 
+// asdasd
+
 mongoose
   .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
